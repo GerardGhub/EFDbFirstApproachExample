fe59385d5ff5c184552d94513f3221f8aff079f2
gerard singian <gerard.singian@outlook.com>
2022-04-15 00:44:59 +0800

    Add project files.

---
+++ EFDbFirstApproachExample/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(EFDbFirstApproachExample.Startup))]
+
+namespace EFDbFirstApproachExample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions() { AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie, LoginPath = new PathString("/Account/Login") });
+  
+        }
+    }
+}
