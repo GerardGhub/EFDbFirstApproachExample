a8a6545d2d7f4aea88d1917ac85eebc9d34f5225
gerard singian <gerard.singian@outlook.com>
2022-04-16 00:40:06 +0800

    USers Module in admin area

---
+++ EFDbFirstApproachExample/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using EFDbFirstApproachExample.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFDbFirstApproachExample.Areas.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Admin/Users/Index
+        public ActionResult Index()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            List<ApplicationUser> existingUsers = db.Users.ToList();
+            return View(existingUsers);
+        }
+    }
+}
