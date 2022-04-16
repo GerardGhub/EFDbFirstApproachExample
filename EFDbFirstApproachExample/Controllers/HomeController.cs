fe3b5071547881fd29d664917979948c988aece3
gerard singian <gerard.singian@outlook.com>
2022-04-17 00:57:27 +0800

    Result Filtering

--- EFDbFirstApproachExample/Controllers/HomeController.cs
+++ EFDbFirstApproachExample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EFDbFirstApproachExample.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +7,8 @@ using System.Web.Mvc;
 
 namespace EFDbFirstApproachExample.Controllers
 {
+    [MyActionFilter]
+    [MyResultFilter]
     public class HomeController : Controller
     {
         public ActionResult Index()
